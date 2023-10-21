8340065d28e86b6e0c285242b6e7677d014003f2
shivansh <shivneeraj2004@gmail.com>
2023-10-21 19:32:20 +0530

    new changes made

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ const addTask = (e) => {
     const task = input.value
     console.log(task)
     const newTask = document.createElement('li')
+    const del = document.createElement('span')
+    del.classList.add('kk')
 
-    newTask.innerText = task;
-    newTask.classList.add('k')
+    newTask.innerHTML = task
+
+    del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`
+
+    newTask.appendChild(del)
+
+
 
     ul.appendChild(newTask)
 
@@ -33,5 +40,18 @@ const doneTask = function (e) {
 
 }
 
+
+// const delTask = (e) => {
+//     if (e.target.tagName === 'SPAN') {
+//         const taskToDelete = e.target.parentElement; // Get the parent <li> element
+//         taskToDelete.remove(); // Remove the task element
+//     }
+// }
+
+const cross = document.querySelector('')
+
+
 ul.addEventListener('click', doneTask)
+cross.addEventListener('click', delTask)
+
 
