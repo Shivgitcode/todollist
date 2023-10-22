aa18c7a33180bf0e0a198c04867115b29f8908d2
shivansh <shivneeraj2004@gmail.com>
2023-10-22 18:27:29 +0530

    the project is now complete made changes in the css and javascript

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 
 const ul = document.querySelector("#list-container")
-// console.log(task)
+let store
 
-const addTask = (e) => {
 
-    console.log(e.target.tagName)
+
+const addTask = () => {
+
+
+
+
 
     const input = document.querySelector("#input-box")
     const task = input.value
-    console.log(task)
-    const newTask = document.createElement('li')
-    const del = document.createElement('span')
-    del.classList.add('kk')
+    if (task === " ") {
+        alert("you have to add some text")
 
-    newTask.innerHTML = task
-
-    del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`
-
-    newTask.appendChild(del)
+    }
+    else {
+        const newTask = document.createElement('li')
+        const del = document.createElement('span')
 
 
+        newTask.innerHTML = task
 
-    ul.appendChild(newTask)
+        del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`
+
+        newTask.appendChild(del)
+
+
+
+        ul.appendChild(newTask)
+        input.value = ""
+
+    }
+
+
 
 }
 
@@ -34,24 +47,28 @@ but1.addEventListener('click', addTask)
 
 
 const doneTask = function (e) {
+    console.log(e.target)
+
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle('checked')
+
+    }
+    else if (e.target.tagName === 'svg') {
+        e.target.parentElement.parentElement.remove()
+        console.log(e.target.parentElement)
     }
 
 }
 
 
-// const delTask = (e) => {
-//     if (e.target.tagName === 'SPAN') {
-//         const taskToDelete = e.target.parentElement; // Get the parent <li> element
-//         taskToDelete.remove(); // Remove the task element
-//     }
-// }
 
-const cross = document.querySelector('')
+
+
+
+
 
 
 ul.addEventListener('click', doneTask)
-cross.addEventListener('click', delTask)
+
 
 
